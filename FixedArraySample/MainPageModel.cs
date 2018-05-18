79691c75fb797ce56df78ceae14e1bb12280aab6
Gerald Versluis <gerald@verslu.is>
2018-05-18 10:46:52 +0200

    Initial commit

---
+++ FixedArraySample/MainPageModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace FixedArraySample
+{
+	public class MainPageModel
+	{
+		public List<bool> Days { get; set; } = new List<bool>
+		{
+			false,
+			false,
+			true,
+			false,
+			false,
+			false,
+			true
+		};
+	}
+}
